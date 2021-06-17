2bcf709391815746d6c26ab392b395d2f05d060d
denamyte <denamyte@gmail.com>
2021-06-18 02:48:36 +0300

    2021-06-18: Stage 1/5

--- Rock-Paper-Scissors/task/rps/game.py
+++ Rock-Paper-Scissors/task/rps/game.py
@@ -1,1 +1,14 @@
-# Write your code here
+class RPS:
+    dependency_list = ['rock', 'paper', 'scissors']
+
+    def __init__(self):
+        pass
+
+    def cheat_move(self):
+        user_choice = input()
+        i = self.dependency_list.index(user_choice)
+        win_index = (i + 1) % len(self.dependency_list)
+        print(f'Sorry, but the computer chose {self.dependency_list[win_index]}')
+
+
+RPS().cheat_move()
