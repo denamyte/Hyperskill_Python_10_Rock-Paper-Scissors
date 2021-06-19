f64add2693537353504ecd7f9777389145381b73
denamyte <denamyte@gmail.com>
2021-06-20 01:12:42 +0300

    2021-06-20: Stage 2/5

--- Rock-Paper-Scissors/task/rps/game.py
+++ Rock-Paper-Scissors/task/rps/game.py
@@ -1,14 +1,44 @@
+import random
+
+
 class RPS:
-    dependency_list = ['rock', 'paper', 'scissors']
+    DEPENDENCY_LIST = ['rock', 'paper', 'scissors']
+    SIZE = len(DEPENDENCY_LIST)
+    STATE_DRAW, STATE_WIN, STATE_LOSE = range(3)
+    MESSAGES = {STATE_DRAW: 'There is a draw ({})',
+                STATE_WIN: 'Well done. The computer chose {} and failed',
+                STATE_LOSE: 'Sorry, but the computer chose {}'}
 
     def __init__(self):
-        pass
+        self.user_input = ''
+        self.comp_input = ''
+        self.state = self.STATE_DRAW
 
-    def cheat_move(self):
-        user_choice = input()
-        i = self.dependency_list.index(user_choice)
-        win_index = (i + 1) % len(self.dependency_list)
-        print(f'Sorry, but the computer chose {self.dependency_list[win_index]}')
+    def set_user_input(self, user_input):
+        self.user_input = user_input
+
+    def choose_computer_input(self):
+        self.comp_input = random.choice(self.DEPENDENCY_LIST)
+
+    def calc_result(self):
+        user_ind = self.DEPENDENCY_LIST.index(self.user_input)
+        comp_ind = self.DEPENDENCY_LIST.index(self.comp_input)
+        self.state =\
+            self.STATE_DRAW if user_ind == comp_ind else\
+            self.STATE_LOSE if (user_ind + 1) % self.SIZE == comp_ind else\
+            self.STATE_WIN
+
+    def __str__(self):
+        return self.MESSAGES.get(self.state).format(self.comp_input)
 
 
-RPS().cheat_move()
+def gameplay():
+    rps = RPS()
+    rps.set_user_input(input())
+    rps.choose_computer_input()
+    rps.calc_result()
+
+    print(rps)
+
+
+gameplay()
