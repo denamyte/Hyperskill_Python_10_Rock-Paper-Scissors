03219d357fe83d0a2be487e02508a215c51daffe
denamyte <denamyte@gmail.com>
2021-06-27 22:57:24 +0300

    2021-06-27: Stage 4/5

--- Rock-Paper-Scissors/task/rps/game.py
+++ Rock-Paper-Scissors/task/rps/game.py
@@ -1,59 +1,87 @@
 import random
+from typing import Dict
 
 
 class RPS:
     DEPENDENCY_LIST = ['rock', 'paper', 'scissors']
     SIZE = len(DEPENDENCY_LIST)
     STATE_DRAW, STATE_WIN, STATE_LOSE = range(3)
-    MESSAGES = {STATE_DRAW: 'There is a draw ({})',
-                STATE_WIN: 'Well done. The computer chose {} and failed',
-                STATE_LOSE: 'Sorry, but the computer chose {}'}
+    MESSAGES: Dict[int, str] = {STATE_DRAW: 'There is a draw ({})',
+                                STATE_WIN: 'Well done. The computer chose {} and failed',
+                                STATE_LOSE: 'Sorry, but the computer chose {}'}
+    POINTS: Dict[int, int] = {STATE_DRAW: 50,
+                              STATE_WIN: 100}
+    FILE_NAME = 'rating.txt'
 
     def __init__(self):
-        self.user_input = ''
-        self.comp_input = ''
-        self.state = self.STATE_DRAW
+        self._user_name: str = ''
+        self._user_rating: int = 0
+        self._user_input: str = ''
+        self._comp_input: str = ''
+        self._state: int = self.STATE_DRAW
 
-    def set_valid_user_input(self, user_input) -> bool:
-        if user_input not in self.DEPENDENCY_LIST:
-            return False
-        self.user_input = user_input
-        return True
+    def read_user_name_and_greet(self):
+        self._user_name = input('Enter your name: ')
+        print(f'Hello, {self._user_name}')
 
-    def set_user_input(self, user_input):
-        self.user_input = user_input
+    def read_rating_file(self):
+        with(open(self.FILE_NAME, 'r')) as file:
+            ratings = {k: int(v) for k, v in (line.split() for line in file.readlines())}
+            self._user_rating = ratings.get(self._user_name, 0)
+
+    @property
+    def user_rating(self):
+        return self._user_rating
+
+    @property
+    def user_input(self):
+        return self._user_input
+
+    @user_input.setter
+    def user_input(self, value: str):
+        self._user_input = value
 
     @staticmethod
     def option_is_valid(option: str):
         return option in RPS.DEPENDENCY_LIST
 
     def choose_computer_input(self):
-        self.comp_input = random.choice(self.DEPENDENCY_LIST)
+        self._comp_input = random.choice(self.DEPENDENCY_LIST)
 
     def calc_result(self):
-        user_ind = self.DEPENDENCY_LIST.index(self.user_input)
-        comp_ind = self.DEPENDENCY_LIST.index(self.comp_input)
-        self.state =\
-            self.STATE_DRAW if user_ind == comp_ind else\
-            self.STATE_LOSE if (user_ind + 1) % self.SIZE == comp_ind else\
+        user_ind = self.DEPENDENCY_LIST.index(self._user_input)
+        comp_ind = self.DEPENDENCY_LIST.index(self._comp_input)
+        self._state = \
+            self.STATE_DRAW if user_ind == comp_ind else \
+            self.STATE_LOSE if (user_ind + 1) % self.SIZE == comp_ind else \
             self.STATE_WIN
 
+    def update_user_rating(self):
+        self._user_rating += self.POINTS.get(self._state, 0)
+
     def __str__(self):
-        return self.MESSAGES.get(self.state).format(self.comp_input)
+        return self.MESSAGES.get(self._state).format(self._comp_input)
 
 
 def gameplay():
+    rps = RPS()
+    rps.read_user_name_and_greet()
+    rps.read_rating_file()
+
     while True:
         inp = input()
         if inp == '!exit':
             break
+        if inp == '!rating':
+            print('Your rating:', rps.user_rating)
+            continue
         if not RPS.option_is_valid(inp):
             print('Invalid input')
             continue
-        rps = RPS()
-        rps.set_user_input(inp)
+        rps.user_input = inp
         rps.choose_computer_input()
         rps.calc_result()
+        rps.update_user_rating()
         print(rps)
 
     print('Bye!')
