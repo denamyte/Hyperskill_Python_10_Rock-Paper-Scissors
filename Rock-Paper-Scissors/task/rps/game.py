71429b6f986093a566f9fb7eb3f2bf152fbecefc
denamyte <denamyte@gmail.com>
2021-06-30 00:45:57 +0300

    2021-06-30: Stage 5/5

--- Rock-Paper-Scissors/task/rps/game.py
+++ Rock-Paper-Scissors/task/rps/game.py
@@ -1,10 +1,9 @@
 import random
-from typing import Dict
+from typing import Dict, List
 
 
 class RPS:
-    DEPENDENCY_LIST = ['rock', 'paper', 'scissors']
-    SIZE = len(DEPENDENCY_LIST)
+    DEFAULT_OPTIONS = ['rock', 'paper', 'scissors']
     STATE_DRAW, STATE_WIN, STATE_LOSE = range(3)
     MESSAGES: Dict[int, str] = {STATE_DRAW: 'There is a draw ({})',
                                 STATE_WIN: 'Well done. The computer chose {} and failed',
@@ -19,6 +18,9 @@ class RPS:
         self._user_input: str = ''
         self._comp_input: str = ''
         self._state: int = self.STATE_DRAW
+        self._options: List[str] = []
+        self._half_size = 0
+        self._size = 0
 
     def read_user_name_and_greet(self):
         self._user_name = input('Enter your name: ')
@@ -28,6 +30,12 @@ class RPS:
         with(open(self.FILE_NAME, 'r')) as file:
             ratings = {k: int(v) for k, v in (line.split() for line in file.readlines())}
             self._user_rating = ratings.get(self._user_name, 0)
+
+    def read_options(self):
+        inp = input()
+        self._options = inp.split(',') if inp else self.DEFAULT_OPTIONS[:]
+        self._size = len(self._options)
+        self._half_size = self._size // 2
 
     @property
     def user_rating(self):
@@ -41,20 +49,24 @@ class RPS:
     def user_input(self, value: str):
         self._user_input = value
 
-    @staticmethod
-    def option_is_valid(option: str):
-        return option in RPS.DEPENDENCY_LIST
+    def option_is_valid(self, option: str):
+        return option in self._options
 
     def choose_computer_input(self):
-        self._comp_input = random.choice(self.DEPENDENCY_LIST)
+        self._comp_input = random.choice(self._options)
 
     def calc_result(self):
-        user_ind = self.DEPENDENCY_LIST.index(self._user_input)
-        comp_ind = self.DEPENDENCY_LIST.index(self._comp_input)
+        user_ind = self._options.index(self._user_input)
+        comp_ind = self._options.index(self._comp_input)
         self._state = \
             self.STATE_DRAW if user_ind == comp_ind else \
-            self.STATE_LOSE if (user_ind + 1) % self.SIZE == comp_ind else \
-            self.STATE_WIN
+            self.calc_complex_result(user_ind, comp_ind)
+
+    def calc_complex_result(self, user_ind, comp_ind) -> int:
+        diff = comp_ind - user_ind
+        if diff < 0:
+            diff += self._size
+        return self.STATE_LOSE if diff <= self._half_size else self.STATE_WIN
 
     def update_user_rating(self):
         self._user_rating += self.POINTS.get(self._state, 0)
@@ -67,6 +79,8 @@ def gameplay():
     rps = RPS()
     rps.read_user_name_and_greet()
     rps.read_rating_file()
+    rps.read_options()
+    print("Okay, let's start")
 
     while True:
         inp = input()
@@ -75,7 +89,7 @@ def gameplay():
         if inp == '!rating':
             print('Your rating:', rps.user_rating)
             continue
-        if not RPS.option_is_valid(inp):
+        if not rps.option_is_valid(inp):
             print('Invalid input')
             continue
         rps.user_input = inp
